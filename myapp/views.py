6eb4d0fcb364dd463612133d3308202463ca7a94
sasakiroo <muhammadrobisasaki@gmail.com>
2022-11-12 12:55:41 +0800

    add files

--- myapp/views.py
+++ myapp/views.py
@@ -1,12 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from .models import ToDoList, Item
+from .forms import CreateNewList
 # Create your views here.
 
 def index(response, id):
 	ls = ToDoList.objects.get(id=id) 
+
+	if response.method == "POST":
+		print(response.POST)
+		if response.POST.get("save"):
+			for item in ls.item_set.all():
+				if response.POST.get("c" + str(item.id)) == "clicked":
+					item.complete = True
+				else:
+					item.complete = False
+
+				item.save()
+
+
+		elif response.POST.get("newItem"):
+			text = response.POST.get("new_input")
+			if len(text) > 2:
+				ls.item_set.create(text=text, complete=False)
+			else:
+				print("you must enter atleast 3 characters")
+
+			
+
 	return render(response, 'myapp/list.html', {"ls":ls})
 
 def home(response):
 	return render(response, 'myapp/home.html', {})
+
+def create(response):
+	if response.method == 'POST':
+		form = CreateNewList(response.POST)
+
+		if form.is_valid():
+			n = form.cleaned_data["name"]
+			t = ToDoList(name=n)
+			t.save()
+
+		return HttpResponseRedirect("/%i" %t.id)
+
+	else:
+		form = CreateNewList()
+	return render(response, 'myapp/create.html', {"form": form})
 
